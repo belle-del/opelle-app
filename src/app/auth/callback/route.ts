416b2c7b527c9ebe095294e30ef98d1c5e4d398d
belle-del <belle@dominusfoundry.com>
2026-01-03 14:13:19 -0700

    fix

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,39 +1,43 @@
-import { NextResponse } from "next/server";
 import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+import { type NextRequest } from "next/server";
 
-export async function GET(request: Request) {
-  const url = new URL(request.url);
-  const code = url.searchParams.get("code");
-  const nextParam = url.searchParams.get("next") ?? "/";
+export async function GET(request: NextRequest) {
+  const { searchParams, origin } = new URL(request.url);
+  const code = searchParams.get("code");
+  const next = searchParams.get("next") ?? "/";
 
-  if (!code) {
-    return NextResponse.redirect(new URL("/auth/auth-code-error", url.origin));
-  }
+  if (code) {
+    const cookieStore = await cookies();
 
-  try {
-    const response = NextResponse.next();
     const supabase = createServerClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "",
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
       {
         cookies: {
           getAll() {
-            return request.cookies.getAll();
+            return cookieStore.getAll();
           },
           setAll(cookiesToSet) {
-            cookiesToSet.forEach(({ name, value, options }) => {
-              response.cookies.set(name, value, options);
-            });
+            try {
+              cookiesToSet.forEach(({ name, value, options }) =>
+                cookieStore.set(name, value, options)
+              );
+            } catch {
+              // The `setAll` method was called from a Server Component.
+            }
           },
         },
       }
     );
+
     const { error } = await supabase.auth.exchangeCodeForSession(code);
-    if (error) {
-      return NextResponse.redirect(new URL("/auth/auth-code-error", url.origin));
+
+    if (!error) {
+      return NextResponse.redirect(`${origin}${next}`);
     }
-    return NextResponse.redirect(new URL(nextParam, url.origin));
-  } catch {
-    return NextResponse.redirect(new URL("/auth/auth-code-error", url.origin));
   }
+
+  return NextResponse.redirect(`${origin}/auth/auth-code-error`);
 }
